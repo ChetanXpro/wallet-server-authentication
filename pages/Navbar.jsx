bb1a965576d25c7bb179f6b3312e9002bf04a6c2
Chetan baliyan <chetanbaliyan10@gmail.com>
2023-04-21 23:25:51 +0530

    init

---
+++ pages/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ConnectButton,getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+const Navbar = () => {
+    
+  return (
+    <div className='mx-auto  flex p-4 items-center justify-end'>l</div>
+  )
+}
+
+export default Navbar
